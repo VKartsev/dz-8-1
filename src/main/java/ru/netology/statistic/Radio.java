f86ca3f751773f9548713a94eac9431998a0fb65
Валерий Карцев <vach-partner@mail.ru>
2022-07-21 21:14:19 +0300

    Update Radio.java

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -1,19 +1,19 @@
 package ru.netology.statistic;
 
 public class Radio {
-    private int currentStation;
-    private int currentVolume;
+    private int currentStation = 0;
+    private int currentVolume = 9;
 
-    public void setCurrentStation(int currentStation) {
+//public void setCurrentStation(int currentStation) {
 
-        if (currentStation < 0) {
-            return;
-        }
-        if (currentStation > 9) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
+ //       if (currentStation < 0) {
+  //          return;
+  //      }
+ //       if (currentStation > 9) {
+   //         return;
+  //      }
+   //     this.currentStation = currentStation;
+  //  }
 
     public int getCurrentStation() {
         return currentStation;
